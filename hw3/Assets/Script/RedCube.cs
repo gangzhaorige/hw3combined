01ee4c83b457fdca9709648cbf88eb51c2d338de
gangzhaorige <45158992+gangzhaorige@users.noreply.github.com>
2020-02-27 02:05:38 -0800

    hw abce uploaded

---
+++ hw3/Assets/Script/RedCube.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class RedCube : MonoBehaviour
+{
+   public Rigidbody cube;
+   public float moveForce = 10f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if( Input.GetKey("a"))
+        {
+            cube.AddForce(-moveForce, 0, 0);
+        }
+        if( Input.GetKey("d"))
+        {
+            cube.AddForce(moveForce, 0, 0);
+        }
+        if( Input.GetKey("w"))
+        {
+            cube.AddForce(0, 0, moveForce);
+        }
+        if( Input.GetKey("s"))
+        {
+            cube.AddForce(0, 0, -moveForce);
+        }
+    }
+
+     void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "wall")
+        {
+            Text.txt.SetActive(true);
+        }
+    }
+
+     void OnCollisionExit(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "wall")
+        {
+            Text.txt.SetActive(false);
+        }
+    }
+}
---
+++ hw3/Assets/Script/Text.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+    public static GameObject txt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt = GameObject.Find("Text");
+        txt.SetActive(false);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
